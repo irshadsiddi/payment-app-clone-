7d46db0e3c5d3efcebecd4b8729969ea8d60e9dc
Mohammad Irshad Siddi <mohammadirshadsiddi@gmail.com>
2024-05-25 16:58:51 +0530

    added appbar and bottom navigation bar and got an missing initial state error i should resolve that

--- Desktop/payment-app-clone--main/paymentapp/payment_app/lib/homepage.dart
+++ Desktop/payment-app-clone--main/paymentapp/payment_app/lib/homepage.dart
@@ -1,28 +1,63 @@
-/*import 'package:flutter/material.dart';
-
-class MyHomepage extends StatelessWidget {
-  const MyHomepage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-    );
-  }
-}*/
-// gpt checking
 import 'package:flutter/material.dart';
 
-class MyHomepage extends StatelessWidget {
-  const MyHomepage({Key? key}) : super(key: key);
+class MyHomepage extends StatefulWidget {
+  MyHomepage({Key? key}) : super(key: key);
 
+  @override
+  State<MyHomepage> createState() => _MyHomepageState();
+}
+
+class _MyHomepageState extends State<MyHomepage> {
+  int barindex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('Welcome to the homepage!'),
+      appBar: AppBar(
+        backgroundColor: Colors.purple[600],
+        elevation: 0,
+        // profile in icon button
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.account_circle_sharp,
+            color: Colors.white,
+            size: 30,
+          ),
+        ),
+
+        // right side top action buttons
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.qr_code_scanner,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.notifications,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+        ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            setState(() {
+              barindex = index;
+            });
+          },
+          currentIndex: barindex,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.history), label: "Home"),
+          ]),
     );
   }
 }
