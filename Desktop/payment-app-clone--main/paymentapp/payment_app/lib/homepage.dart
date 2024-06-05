f6f0a53c9deecea89e93922578531b1e04eb2831
Mohammad Irshad Siddi <mohammadirshadsiddi@gmail.com>
2024-06-05 23:41:50 +0530

    modified the ui of the user details page

--- Desktop/payment-app-clone--main/paymentapp/payment_app/lib/homepage.dart
+++ Desktop/payment-app-clone--main/paymentapp/payment_app/lib/homepage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:payment_app/phonenumber_verification.dart';
+//import 'package:payment_app/phonenumber_verification.dart';
 
 class MyHomepage extends StatefulWidget {
   MyHomepage({Key? key}) : super(key: key);
